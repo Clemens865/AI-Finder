a5c79734b8cc24d98a278f2925c81b6c2688aa4e
Clemens865 <clemens_hoenig@hotmail.com>
2025-11-02 16:03:49 +0100

    Add build and test configuration files

---
+++ jest.config.cjs
@@ -0,0 +1,53 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@tests/(.*)$': '<rootDir>/tests/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/tests/__mocks__/fileMock.js'
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+          moduleResolution: 'node'
+        }
+      }
+    ]
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(nanoid|uuid)/)'
+  ],
+  roots: ['<rootDir>/tests', '<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{ts,tsx}',
+    '!src/**/__tests__/**',
+    '!src/**/types.ts',
+    '!src/main/index.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75
+    }
+  },
+  coverageReporters: ['text', 'html', 'lcov', 'json-summary'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testTimeout: 30000,
+  maxWorkers: '50%'
+};
